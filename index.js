559bd52b935f1915078809a25ae72b8a18805e9a
Poorna Chandu Gade <gadechandu123@gmail.com>
2017-07-31 16:52:06 +0530

    add

--- index.js
+++ index.js
@@ -2,16 +2,20 @@
 $(function(){
   $(".ham,#close-menu").click(function(){
   if(x==1){
-    $(".content").css({"display":"inherit"});
-    $(".content").removeClass("bounceOutRight");
-    $(".content").addClass("slideInRight");
+    $(".content").css({"display":"inherit"})
+                 .removeClass("bounceOutRight")
+                 .addClass("slideInRight");
     $("#close-menu").addClass("close-overlay");
+    $(".ham .menu").addClass("fadeOutRight").removeClass("fadeInRight");
+    $(".ham .closed").css({"visibility":"visible"}).addClass("fadeInRight").removeClass("fadeOutRight");
       x=0;
     }
   else{
-    $(".content").addClass("bounceOutRight");
-    $(".content").removeClass("slideInRight");
+    $(".content").addClass("bounceOutRight")
+                 .removeClass("slideInRight");
     $("#close-menu").removeClass("close-overlay");
+    $(".ham .menu").addClass("fadeInRight").removeClass("fadeOutRight");
+    $(".ham .closed").addClass("fadeOutRight").removeClass("fadeInRight");
       x=1;
     }
   });
