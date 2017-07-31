64b04f1e639db82b61e67d8ec45af684541e7d8c
Poorna Chandu Gade <gadechandu123@gmail.com>
2017-07-31 14:57:32 +0530

    add

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
   var x = 1;
 $(function(){
-  $(".ham").click(function(){
+  $(".ham,#close-menu").click(function(){
   if(x==1){
     $(".content").css({"display":"inherit"});
     $(".content").removeClass("bounceOutRight");
     $(".content").addClass("slideInRight");
+    $("#close-menu").addClass("close-overlay");
       x=0;
     }
   else{
     $(".content").addClass("bounceOutRight");
     $(".content").removeClass("slideInRight");
+    $("#close-menu").removeClass("close-overlay");
       x=1;
     }
   });
