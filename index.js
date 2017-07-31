79318e454d74b9148d8cafd5b9ad47138f513fe1
Poorna Chandu Gade <gadechandu123@gmail.com>
2017-07-31 23:17:13 +0530

    responsive and fix

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
   var x = 1;
 $(function(){
-  $(".ham,#close-menu").click(function(){
+  $("#nav-icon,#close-menu").click(function(){
   if(x==1){
     $(".content").css({"display":"inherit"})
                  .removeClass("bounceOutRight")
                  .addClass("slideInRight");
     $("#close-menu").addClass("close-overlay");
-    $(".ham .menu").addClass("fadeOutRight").removeClass("fadeInRight");
-    $(".ham .closed").css({"visibility":"visible"}).addClass("fadeInRight").removeClass("fadeOutRight");
       x=0;
     }
   else{
     $(".content").addClass("bounceOutRight")
                  .removeClass("slideInRight");
     $("#close-menu").removeClass("close-overlay");
-    $(".ham .menu").addClass("fadeInRight").removeClass("fadeOutRight");
-    $(".ham .closed").addClass("fadeOutRight").removeClass("fadeInRight");
       x=1;
     }
+    $("#nav-icon").toggleClass('open');
   });
 });
+// $(document).ready(function(){
+// 	$('#nav-icon').click(function(){
+// 		$(this).toggleClass('open');
+// 	});
+// });
