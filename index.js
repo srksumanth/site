075a0ddb671bec0d155fef189500ca7c41b77cc9
Poorna Chandu Gade <gadechandu123@gmail.com>
2017-08-07 17:41:25 +0530

    modified nav

--- index.js
+++ index.js
@@ -1,24 +1,78 @@
   var x = 1;
-$(function(){
-  $("#nav-icon,#close-menu").click(function(){
-  if(x==1){
-    $(".content").css({"display":"inherit"})
-                 .removeClass("bounceOutRight")
-                 .addClass("slideInRight");
-    $("#close-menu").addClass("close-overlay");
-      x=0;
-    }
-  else{
-    $(".content").addClass("bounceOutRight")
-                 .removeClass("slideInRight");
-    $("#close-menu").removeClass("close-overlay");
-      x=1;
-    }
-    $("#nav-icon").toggleClass('open');
+  $(function() {
+    $("#nav-icon,#close-menu").click(function() {
+      if (x == 1) {
+        $(".content").css({
+            "display": "inherit"
+          })
+          .removeClass("slideOutLeft")
+          .addClass("slideInLeft");
+        $("#close-menu").addClass("close-overlay");
+        x = 0;
+      } else {
+        $(".content").addClass("slideOutLeft")
+          .removeClass("slideInLeft");
+        $("#close-menu").removeClass("close-overlay");
+        x = 1;
+      }
+      $("#nav-icon").toggleClass('open');
+    });
+    // projects click
+    $("#projects").click(function() {
+        $(".p1").each(function(i) {
+          var $item = $(this);
+          setTimeout(function() {
+            $item.removeClass("fadeInLeft").addClass("fadeOutLeft");
+            console.log(i);
+          }, 50 * i);
+          setTimeout(function(){
+            $(".p1").css({
+              "display": "none"
+            });
+          },600);
+        });
+        setTimeout(function(){
+        $(".projects-sub").each(function(i) {
+          var $item = $(this);
+          setTimeout(function() {
+            $item.removeClass("fadeOutLeft")
+              .addClass("fadeInLeft")
+              .css({
+                "display": "inherit"
+              });
+          }, 50 * i);
+        });
+      },650);
+    });
+    // project back click
+    $("#p-back").click(function() {
+        $(".projects-sub").each(function(i) {
+          var $item = $(this);
+          setTimeout(function() {
+            $item.removeClass("fadeInLeft").addClass("fadeOutLeft");
+          }, 50 * i);
+        });
+        setTimeout(function(){
+          $(".projects-sub").css({
+            "display": "none"
+          })
+        },400);
+        setTimeout(function(){
+        $(".content div").each(function(i) {
+          var $item = $(".p1");
+          setTimeout(function() {
+            $item.removeClass("fadeOutLeft").addClass("fadeInLeft").css({
+              "display": "inherit"
+            });
+          }, 50 * i);
+        });
+      },450);
+
+
+    });
+
+
+
+
+
   });
-});
-// $(document).ready(function(){
-// 	$('#nav-icon').click(function(){
-// 		$(this).toggleClass('open');
-// 	});
-// });
